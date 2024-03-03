ef599c3bb28cf923bb7532bda60fe67c77643c72
Mizrab Sheikh <mizrabsheikh57@gmail.com>
2024-03-04 02:38:15 +0500

    chore: add required functions definitions

---
+++ my_package/block_functions.go
@@ -0,0 +1,18 @@
+package my_package
+
+import "fmt"
+
+func DisplayAllBlocks() {
+	fmt.Println("Displaying all blocks...")
+
+}
+
+func NewBlock() {
+	fmt.Println("Creating a new block...")
+
+}
+
+func ModifyBlock() {
+	fmt.Println("Modifying an existing block...")
+
+}
