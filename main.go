e79ebd1ee5764d43d9dc3690af49cdc706e15cea
Mizrab Sheikh <mizrabsheikh57@gmail.com>
2024-03-04 02:38:56 +0500

    chore: add entry point file

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"my_project/my_package"
+)
+
+func main() {
+	// Now you can use the functions
+	my_package.DisplayAllBlocks()
+	my_package.NewBlock()
+	my_package.ModifyBlock()
+}
