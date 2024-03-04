7826a744b2ce889507ebdcb74c6f2c86b5789f1e
Mizrab Sheikh <mizrabsheikh57@gmail.com>
2024-03-04 20:46:10 +0500

    chore: create sample blockchain using custom blockchain package

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"my_project/my_package"
+	"blockchain"
+	"fmt"
+	"time"
 )
 
 func main() {
-	// Now you can use the functions
-	my_package.DisplayAllBlocks()
-	my_package.NewBlock()
-	my_package.ModifyBlock()
+	genesisBlock := &blockchain.Block{
+		Index:     0,
+		Timestamp: time.Now().String(),
+		Data:      "Genesis Block",
+		PrevHash:  "",
+	}
+	genesisBlock.Hash = blockchain.calculateHash(genesisBlock)
+
+	blockchain := &blockchain.Blockchain{Blocks: []*blockchain.Block{genesisBlock}}
+
+	blockchain.NewBlock("Transaction 1")
+	blockchain.NewBlock("Transaction 2")
+
+	fmt.Println("All Blocks:")
+	blockchain.DisplayAllBlocks()
+
+	fmt.Println("Modifying Block 1...")
+	blockchain.ModifyBlock(1, "Modified Data")
+
+	fmt.Println("\nAll Blocks after modification:")
+	blockchain.DisplayAllBlocks()
 }
